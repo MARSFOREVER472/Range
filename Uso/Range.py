90581a2bab2140c3091da26e34f164d57370e35d
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-02-16 23:48:28 -0300

    Comienzo del proyecto...

---
+++ Uso/Range.py
@@ -0,0 +1,22 @@
+# USO DEL RANGE:
+
+# Uno de las iteraciones mas comunes que se realizan, es la de iterar un número entre por ejemplo 0 y n. 
+# Si ya programas, estoy seguro de que estas cansado de escribir esto, aunque sea en otro lenguaje. 
+# Pongamos que queremos iterar una variable i de 0 a 5. 
+# Haciendo uso de lo que hemos visto anteriormente, podríamos hacer lo siguiente:
+
+# EJEMPLO PRÁCTICO:
+
+# DATOS DE ENTRADA:
+
+for i in (0, 1, 2, 3, 4, 5):
+    print(i)
+
+# DATOS DE SALIDA:
+
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
