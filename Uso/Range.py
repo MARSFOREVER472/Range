b06c877a6502c0ef985efd55e7b6cb9bfbbb613f
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-02-17 23:34:19 -0300

    Agregando más ejemplos prácticos...

--- Uso/Range.py
+++ Uso/Range.py
@@ -20,3 +20,52 @@ for i in (0, 1, 2, 3, 4, 5):
 # 3
 # 4
 # 5
+
+# Se trata de una solución que cumple con nuestro requisito. 
+# El contenido después del in se trata de una clase que como ya hemos visto antes, es iterable, y es de hecho una tupla. 
+# Sin embargo, hay otras formas de hacer esto en Python, haciendo uso del range().
+
+# EJEMPLO:
+
+# DATOS DE ENTRADA:
+
+for i in range(6):
+    print(i)
+
+# DATOS DE SALIDA:
+
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+
+# El range() genera una secuencia de números que van desde 0 por defecto hasta el número que se pasa como parámetro menos 1. 
+# En realidad, se pueden pasar hasta tres parámetros separados por coma, donde el primer es el inicio de la secuencia, el segundo el final y el tercero el salto que se desea entre números. 
+# Por defecto se empieza en 0 y el salto es de 1.
+
+# EJEMPLO:
+
+# range(inicio, fin, salto)
+
+# Por lo tanto, si llamamos a range() con (5,20,2), se generarán números de 5 a 20 de dos en dos. 
+# Un truco es que el range() se puede convertir en list.
+
+# EJEMPLO:
+
+# DATOS DE ENTRADA:
+
+for i in range(5, 20, 2):
+    print(i)
+
+# DATOS DE SALIDA:
+
+# 5
+# 7
+# 9
+# 11
+# 13
+# 15
+# 17
+# 19
